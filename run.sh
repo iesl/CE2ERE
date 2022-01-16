7a113bbf0b9846a18735ce4ec53aae2755166b1c
Eunjeong Hwang <hej78520@gmail.com>
2022-01-16 00:17:22 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,11 +17,6 @@ echo "SLURMTMPDIR="$SLURMTMPDIR
 
 echo "working directory = "$SLURM_SUBMIT_DIR
 
-source /home/tianyiyang/miniconda3/bin/activate
-conda activate event-relation-extraction
-
-export PYTHONPATH="/home/tianyiyang/git/CE2ERE/"
-
 python src/__main__.py --data_dir=data --model bilstm --data_dir data --data_type matres --downsample 0.02 --epochs 50 --lambda_anno 1 --lambda_trans 0 --learning_rate 1e-05 --log_batch_size 6 --lstm_hidden_size 256 --lstm_input_size 768 --mlp_size 512 --roberta_hidden_size 768 --num_layers 1
 #python src/main.py --data_dir=data --model bilstm --embedding_type longformer-base --data_dir data --data_type hieve --downsample 0.02 --epochs 50 --lambda_anno 1 --lambda_trans 0 --learning_rate 1e-05 --log_batch_size 6 --lstm_hidden_size 256 --lstm_input_size 768 --mlp_size 512 --roberta_hidden_size 768 --num_layers 1
 # python src/main.py --data_dir=data --model box --embedding_type roberta-base --data_dir data --data_type hieve --downsample 0.02 --epochs 50 --lambda_anno 1 --lambda_trans 0 --learning_rate 1e-05 --log_batch_size 6 --lstm_hidden_size 256 --lstm_input_size 768 --mlp_size 512 --roberta_hidden_size 768 --num_layers 1
